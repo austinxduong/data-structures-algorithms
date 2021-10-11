168034b8e7744e04b0e6cdeb5f68e1a7b23d67ea
austinxduong <axduong2@gmail.com>
2021-10-10 22:54:51 -0700

    fixed

--- bigONotation.js
+++ bigONotation.js
@@ -1,5 +1,6 @@
 const whatEvenIsBigONotation = 'a way to measure effciency, a way to rate a data structure';
 console.log(whatEvenIsBigONotation);
+// a way to measure effciency, a way to rate a data structure
 
 function theEffciencyAndTheRatingOfWhat() {
     const effciencyOfThese4ThingsInADataStructure = ['acessing', 'searching', 'inserting', 'deleting'];
