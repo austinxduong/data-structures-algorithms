8d4a49175f774626636e433e57b800384fec4bb7
austinxduong <axduong2@gmail.com>
2021-10-11 21:44:54 -0700

    measure time complexity, using Big O notation

--- bigONotation.js
+++ bigONotation.js
@@ -11,5 +11,14 @@ function theEffciencyAndTheRatingOfWhat() {
 }
 theEffciencyAndTheRatingOfWhat();
 // how the data structure is ....  acessing,searching,inserting,deleting
+// *sidenote.... in turn this helps us determine which data structure is "good" at what, or "bad" at what
 
+function butHowDoesBigONotationActuallyMeasureTheseFourThings() {
+    const timeComplexityEquation = 'Time Complexity equation. This is 1 way BigO measures the efficieny of these 4 things';
 
+    if(true) {
+        return console.log(`${timeComplexityEquation}`)
+    }
+}
+butHowDoesBigONotationActuallyMeasureTheseFourThings();
+// Time Complexity equation. This is 1 way BigO measures the efficieny of these 4 things
