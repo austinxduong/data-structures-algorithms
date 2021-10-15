ff135bda62ce591e71ba90d7c256bc4607005b03
austinxduong <axduong2@gmail.com>
2021-10-15 00:09:08 -0700

    loop through an array of strings, by index

--- allAboutForLoops.js
+++ allAboutForLoops.js
@@ -12,18 +12,25 @@ for (let i = 0; i < 4; i++) {
 // i = represents the "index"
 // 4 = represents the "condition"
 // i++ = is our "incrementer"
+
 // this reads, starting at index 0, if index is below 4, keep incrementing
-/*for (starting at index 0; if index is below 4; keep incrementing) 
+/*
+
+for (starting at index 0; if index is below 4; keep incrementing) 
     console.log('lets loop 4 times');
-    }*/
+    } 
+
+*/
 
 for (let i = 0; i < 4; i++) {
     console.log('lets loop 4 times' + i);
 }
-/*  lets loop 4 times0
-    lets loop 4 times1
-    lets loop 4 times2
-    lets loop 4 times3 */
+/*  
+lets loop 4 times0
+lets loop 4 times1
+lets loop 4 times2
+lets loop 4 times3 
+*/
 
 // we can see the their indentified indexes
 
@@ -39,4 +46,12 @@ loop 4 times, buttt wait. lets stop at a specific index 0
 loop 4 times, buttt wait. lets stop at a specific index 1
 loop 4 times, buttt wait. lets stop at a specific index 2
 */
+
+// looping through an array of strings
+// we replace i<5 with "iceCream.length"
+const iceCream = ['strawberry', 'half baked', 'oreo', 'peanut butter', 'classic vanilla'];
+
+for (i = 0; i < iceCream.length; i++) {
+    console.log(iceCream[i] + ' ' + i);
+}
 
