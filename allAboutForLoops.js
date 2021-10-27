2878f15b8d174467d006dbdc403a5fa106065159
austinxduong <axduong2@gmail.com>
2021-10-26 18:57:05 -0700

    refined

--- allAboutForLoops.js
+++ allAboutForLoops.js
@@ -23,7 +23,7 @@ for (starting at index 0; if index is below 4; keep incrementing)
 */
 
 for (let i = 0; i < 4; i++) {
-    console.log('lets loop 4 times' + i);
+    console.log('classic--> lets loop 4 times' + i);
 }
 /*  
 lets loop 4 times0
@@ -31,30 +31,16 @@ lets loop 4 times1
 lets loop 4 times2
 lets loop 4 times3 
 */
-
 // we can see the their indentified indexes
 
-// we can use "break", to stop at specicfic index, like so:
-
-for (let i = 0; i <4 ; i++ ) {
-    console.log("loop 4 times, buttt wait. lets stop at a specific index" + ' ' + i);
-    if (i === 2) break;
-}
-
-/*
-loop 4 times, buttt wait. lets stop at a specific index 0
-loop 4 times, buttt wait. lets stop at a specific index 1
-loop 4 times, buttt wait. lets stop at a specific index 2
-*/
 
 // looping through an array of strings
 // we replace i<5 with "iceCream.length"
 const iceCream = ['strawberry', 'half baked', 'oreo', 'peanut butter', 'classic vanilla'];
 
 for (i = 0; i < iceCream.length; i++) {
-    console.log(iceCream[i] + ' ' + i);
+    console.log("iceCream.length-->",iceCream[i] + ' ' + i);
 }
-
 /*
 strawberry 0
 half baked 1
@@ -63,22 +49,25 @@ peanut butter 3
 classic vanilla 4
 */
 
+// "for-of" loops
+// designed to loop/iterate through arrays
 // we can decontruct the iceCream array 
 // to do that, we just assign a new variable name "from" the name of our array
 // the caveat.... while there is less code written, the "for of" loop is much slower, than the classic for loop used above
-
 const iceCream0 = ['strawberry', 'half baked', 'oreo', 'peanut butter', 'classic vanilla']; {
 
-for (flavors of iceCream0) {
-    console.log(flavors);
-}
+    for (flavors of iceCream0) {
+        console.log("for-of loop-->", flavors);
+    }
 }
 
-// "for in" loop, using an object of key/value pairs
+// "for in" loops 
+// designed to loop/iterate through objects
+// can be used to loop through arrays, but "for-of loops" are designed for arrays
 const plant = {'name': 'stanley', 'type': 'plant', 'age': 2}
 
 for (valueFromKey in plant) {
-    console.log(plant[valueFromKey]);
+    console.log("for-in loops-->",plant[valueFromKey]);
 }
 // below, we just returned our value of the key, from the plant array
 /*
@@ -93,9 +82,8 @@ plant
 
 for (let i = 0; i < 22; i++) {
     if (i === 9) { continue; }
-    console.log("hello im iterating: " + i);
+    console.log("continue--> hello im iterating: " + i);
   }
-
 /*
 hello im iterating: 0
 hello im iterating: 1
@@ -122,7 +110,6 @@ hello im iterating: 21
 
 // "break" statement usually lives inside a conditional statement
 // if the condition is met, the loops stops at the specific condition (element), and removes it self from the iteration.
-
 for (let i = 0; i < 7; i++) {
     if( i === 3 ) { break };
     console.log("break at element 3, and get out of loop", i);
@@ -132,6 +119,17 @@ for (let i = 0; i < 7; i++) {
 break at element 3, and get out of loop 0
 break at element 3, and get out of loop 1
 break at element 3, and get out of loop 2
+*/
+
+// "break" statement, playing/practicing some more 
+for (let i = 0; i <4 ; i++ ) {
+    console.log("loop 4 times, buttt wait. lets break at a specific index" + ' ' + i);
+    if (i === 2) break;
+}
+/*
+loop 4 times, buttt wait. lets stop at a specific index 0
+loop 4 times, buttt wait. lets stop at a specific index 1
+loop 4 times, buttt wait. lets stop at a specific index 2
 */
 
 
