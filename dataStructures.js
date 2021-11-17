cde8ee5e3340662f170be87e53f9473afdb3ef24
austinxduong <axduong2@gmail.com>
2021-11-16 20:14:18 -0800

    just keep goin

--- dataStructures.js
+++ dataStructures.js
@@ -3,3 +3,5 @@ console.log("What are Data Structures Even? -->",dataStructures);
 
 const realLifeExampleDataStructure = 'gmail for example organizes our emails, categorizes our emails using folders, or even more advanced techniques like filters/rules to sort our emails';
 console.log("Whats a practical real life analogy?-->",realLifeExampleDataStructure);
+
+const howIstheDataStructureActuallyStored = 'inside memory slots. in the form of bits. 8 bits equals whats called a byte.'
