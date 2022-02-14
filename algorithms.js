c7ba246530f64c9bd6b498f5f39df22906e8bafe
austinxduong <axduong2@gmail.com>
2022-02-14 13:01:25 -0800

    recursion technique

--- algorithms.js
+++ algorithms.js
@@ -1,2 +1,7 @@
+// all logic lives here
+
 algorithms = "process of doing something, a procedure; computationals steps of taking an input(s), & produces a value. a method."
 console.log(algorithms);
+
+
+
