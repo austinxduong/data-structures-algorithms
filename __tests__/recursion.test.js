593a72fc8b04d83bb510dce68a3462cba370e642
austinxduong <axduong2@gmail.com>
2022-02-17 16:37:57 -0800

    passed tested regex function

--- __tests__/recursion.test.js
+++ __tests__/recursion.test.js
@@ -1,7 +1,12 @@
 const getRandom  = require ('../recursion');
+const regexDemo = require ('../allAboutRegex')
 
 test('finds a integer between 50 and 88, including both inputs', () => {
     
     expect(getRandom(50, 88)).toBeGreaterThanOrEqual(50)
     expect(getRandom(50, 88)).toBeLessThanOrEqual(88)
 });
+
+test('finds all instances that match /"austiMatch/", and returns how many ', () => {
+    expect(regexDemo("Hello is austiMatch here?, There should be two. See austiMatch :)")).toBe(2)
+})
--- allAboutRegex.js
+++ allAboutRegex.js
@@ -1,15 +1,19 @@
 // because i was asked on this during an interview once, and didnt know what I was doing.
 
-const REGEX = /austiMatch/
+// const REGEX = /austiMatch/
 
 // const REGEX = /austiMatch/i 
 
-// const REGEX = /austiMatch/g
+const REGEX = /austiMatch/g
 
 // const REGEX = /austiMatch/ig
 
 function regexDemo(str) {
-    return str.match(REGEX) //<--- im using REGEX heheh
+    return str.match(REGEX).length //<--- im using REGEX heheh
+    
 }
 
-console.log(regexDemo("where did austi go? was austiMatch found using REGEX?. This is s demo! Hiiiiii"))
+module.exports = regexDemo
+
+console.log(regexDemo("where did austi go? was austiMatch found using REGEX?. A second austiMatch is found. This is s demo! Hiiiiii"))
+
